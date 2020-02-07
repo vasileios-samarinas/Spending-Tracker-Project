865c1fc84a157d682947aa6c662a5b2a0f1d6dbd
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-07 14:26:58 +0000

    Moneytracker sql file created

---
+++ db/moneytracker.sql
@@ -0,0 +1,20 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE transactions;
+
+CREATE TABLE merchants(
+  id SERIAL primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE categories(
+  id SERIAL primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE transactions(
+  id SERIAL primary key,
+  category_id INT REFERENCES category(id),
+  merchant_id INT REFERENCES merchant(id),
+  amount INT
+);
