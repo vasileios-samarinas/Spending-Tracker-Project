6b418f26d3a65b2c855a213b28a5ee8baa694336
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-08 17:17:38 +0000

    get /merchants/:id  completed

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -7,3 +7,8 @@ get 'merchants' do
   @merchants=Merchant.all()
   erb(:"merchants/index")
 end
+
+get '/merchants/:id' do
+  @merchant = Merchant.find(params['id'].to_i)
+  erb(:"merchants/show")
+end
