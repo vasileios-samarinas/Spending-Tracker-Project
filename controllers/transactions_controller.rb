eba0e28a80006620a74fe78f81e2e64c04c9261e
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-08 15:05:29 +0000

    Creation of get transactions and relevant index files

---
+++ controllers/transactions_controller.rb
@@ -0,0 +1,12 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/transaction.rb' )
+require_relative( '../models/merchant.rb' )
+require_relative( '../models/category.rb' )
+also_reload( '../models/*' )
+
+get '/transactions' do
+  @transactions = Transaction.all
+  erb ( :"transactions/index" )
+end
