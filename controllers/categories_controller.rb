70dc00857df48ddf72b634c1e262decde61461f6
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-09 16:20:31 +0000

    Category controller index completed

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -1,14 +1,12 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require( 'sinatra/contrib/all')
+require( 'pry' )
 require_relative( '../models/category.rb' )
 also_reload( '../models/*' )
 
-get 'categories' do
+
+#READ - all/index
+get '/categories' do
   @categories=Category.all()
   erb(:"categories/index")
-end
-
-get '/categories/:id' do
-  @category = Category.find(params['id'].to_i)
-  erb(:"categories/show")
 end
